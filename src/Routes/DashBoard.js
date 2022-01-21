d770253a0e693b6d93af17fa2b6a55e2165b3ec2
damii dennison <dwanedehydra@gmail.com>
2022-01-21 21:10:48 +0100

    ScheduleList added

--- src/Routes/DashBoard.js
+++ src/Routes/DashBoard.js
@@ -1,8 +1,10 @@
+import ScheduleList from "./ScheduleList";
 import Subscribe from "./subscribe";
 
 function Dashboard({user}) {
 	return (<>
 		<Subscribe user={user} />
+		<ScheduleList/>
 	</>);
 }
 
---
+++ src/Routes/ScheduleList.js
@@ -0,0 +1,9 @@
+function ScheduleList() {
+    return (
+        <h1>welcome to list</h1>
+    )
+}
+
+
+
+export default ScheduleList;
